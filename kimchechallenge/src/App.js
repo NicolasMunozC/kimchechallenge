2fec1c6fcc872c14d6093ee1e431feaecd1b612d
Nicolas Muñoz Caceres <nicolasmunozcaceres@MacBook-Pro-de-Nicolas.local>
2022-05-13 01:28:20 -0400

    Segunda patita, se agrego la data, y html basico de prueba temporal

--- kimchechallenge/src/App.js
+++ kimchechallenge/src/App.js
@@ -1,22 +1,51 @@
 import React from "react";
 import "./App.css";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks"
 
-const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
-});
+const allCountries = gql`
+query {
+  countries{
+    name
+    continent{
+      name
+    }
+    languages{
+      code
+      name
+    }
+  }
+}
+`
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>
-        My first Apollo app{" "}
-        <span role="img" aria-label="Rocket">
-          🚀
-        </span>
-      </h2>
-    </div>
-  </ApolloProvider>
-);
-export default App;
+function App() {
+  const [searchCountry, setSearchCountry] = React.useState('')
+
+  const {data, error, loading} = useQuery(allCountries)
+
+  if(error){
+    console.log(error);
+  }
+
+  return (
+    <>
+    <h1>Country Search</h1>
+    <p>Hello my friend! :) Please write something below...</p>
+    <input type='text' autoFocus value={searchCountry} onChange={(e)=>{setSearchCountry(e.target.value)}}></input>
+    <p>Group by:</p>
+    <button>Continent</button>
+    <button>Language</button> <br/>
+    {error && <span>Ops! There is a problem finding the information, please try again later...</span>}
+    {loading ? <span>Loading...</span> : 
+      <ul>
+        { 
+            data.countries.map(country => <li key={country.name}>{country.name}</li>)
+        }
+      </ul>
+    }
+
+    </>
+  )
+}
+
+export default App
